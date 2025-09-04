a39fa7d4434f2155e4f47e7130e78327644bea5f
Matas Savickis <savickismatas@gmail.com>
2025-09-04 16:09:56 +0200

    Initial commit

---
+++ src/test/java/com/apad/weather/WeatherApplicationTests.java
@@ -0,0 +1,13 @@
+package com.apad.weather;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeatherApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
